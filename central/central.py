3646930a03dd3c917ea8fdf616ae65146f8c422a
macp3 <maciej.pietras123@gmail.com>
2024-10-02 15:17:21 +0200

    state change by central

--- central/central.py
+++ central/central.py
@@ -1,14 +1,25 @@
 import socket 
 import threading
+import sys
 
 HEADER = 64
-PORT = 5051
 CENTRAL_IP = socket.gethostbyname(socket.gethostname())
-ADDR = (CENTRAL_IP, PORT)
 FORMAT = 'utf-8'
 EXIT = "EXIT"
 
+if len(sys.argv) != 4:
+    print("Wrong number of arguments")
+    exit()
+
+PORT = int(sys.argv[1])
+BROKER_IP = sys.argv[2]
+BROKER_PORT = sys.argv[3]
+ADDR = (CENTRAL_IP, PORT)
+TAXI_IP = ""
+
 taxi_list = []
+
+
 
 def taxi_control():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,6 +28,9 @@ def taxi_control():
 
     while True:
         conn, addr = server.accept()
+
+        global TAXI_IP
+        TAXI_IP = addr[0]
 
         msg = conn.recv(HEADER).decode(FORMAT) 
         if msg:
@@ -31,6 +45,34 @@ def taxi_control():
                 conn.send(f"0".encode(FORMAT))
             conn.close()
 
-# thread_taxi_control = threading.Thread(target=taxi_control)
-# thread_taxi_control.start()
-taxi_control()
+thread_taxi_control = threading.Thread(target=taxi_control)
+thread_taxi_control.start()
+
+def TAXI_ORDER(ORDER, TAXI_ID):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((TAXI_IP, 5051 + int(TAXI_ID)))
+
+    msg = ORDER + " " + TAXI_ID
+    message = msg.encode(FORMAT)
+
+    client.send(message)
+
+
+
+def TAXI_GO(TAXI_ID, DEST):
+    pass
+
+Working = True
+command = ""
+while Working:
+    command = input("$: ")
+    commands = command.split(" ")
+
+    if(command == EXIT):
+        Working = False
+    elif len(commands) == 2 and commands[0] in ("STOP", "RESUME") and commands[1].isdigit():
+        TAXI_ORDER(commands[0], commands[1])
+    elif len(commands) == 3 and commands[0] == "GO" and commands[1].isdigit():
+        TAXI_GO(commands[1], commands[2])
+    else:
+        print("Wrong command")
--- taxi/taxi.py
+++ taxi/taxi.py
@@ -7,7 +7,7 @@ import sys
 HEADER = 64
 PORT = 5050
 TAXI_IP = socket.gethostbyname(socket.gethostname())
-ADDR_CENT = ('192.168.1.137', 5051)
+ADDR_CENT = ('172.27.131.104', 5051)
 FORMAT = 'utf-8'
 EXIT = "EXIT"
 
@@ -18,6 +18,8 @@ if len(sys.argv) != 2:
 
 
 ID = sys.argv[1]
+
+state = "FINAL"
 
 def send(msg):
     message = msg.encode(FORMAT)
@@ -31,56 +33,82 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR_CENT)
 send(str(ID))
 
+def send_taxi_state():
+        while True:
+            print(ID + " " + state)
+            #producer.send("taxi_status", (ID + " " + state).encode(FORMAT))
+            time.sleep(1)
+
+def taxi_warning(msg, sensor_id):
+    print(f"Warning from sensor number {sensor_id}: {msg}")
+
+    if msg != "HELLO":
+        global state
+        state = "STOPPED"
+
+
+def handle_sensor(conn, sensor_id):
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == EXIT:
+                connected = False
+            else:
+                taxi_warning(msg, sensor_id)
+                conn.send(f"TAXI has been warned".encode(FORMAT))
+    print(f"CLOSING THE SENSOR NUMBER {sensor_id}")
+    conn.close()
+
+def handle_central(conn, addr):
+    connected = True
+    while connected:
+        msg = conn.recv(HEADER).decode(FORMAT)
+        if msg:
+                
+            mes = msg.split(" ")
+            global state
+                
+            if mes[0] == "RESUME":
+                state = "FINAL"
+            elif mes[0] == "STOP":
+                state = "STOPPED"
+            elif mes[0] == "GO":
+                TAXI_go(mes[2])
+
+
+            connected = False
+    conn.close()
+
+def TAXI_go(dest):
+    pass
+
+def start():
+    server.listen()
+    sensor_id = 1
+
+    while True:
+        conn, addr = server.accept()
+
+        # MOZE NIE DZIALAC
+        if addr[0] == ADDR_CENT[0]:
+            thread_cen = threading.Thread(target=handle_central, args=(conn, addr))
+            thread_cen.start()
+        else:
+            thread = threading.Thread(target=handle_sensor, args=(conn, sensor_id))
+            thread.start()
+            num_of_sensors = threading.active_count() - 2
+            sensor_id+=1
+            print(f"Number of sensors: {num_of_sensors}")
+
 ####################################################
 
 if int(client.recv(2048).decode(FORMAT)):
 
     ADDR = (TAXI_IP, PORT+int(ID)+1)
 
-    state = "OK"
-
-    def send_taxi_state():
-        while True:
-            print(state)
-            #producer.send("taxi_status", (ID + " " + state).encode(FORMAT))
-            time.sleep(1)
-
-    def taxi_warning(msg, sensor_id):
-        print(f"Warning from sensor number {sensor_id}: {msg}")
-
-        if msg != "HELLO":
-            global state
-            state = "KO"
-
-
-
-    def handle_sensor(conn, sensor_id):
-        connected = True
-        while connected:
-            msg_length = conn.recv(HEADER).decode(FORMAT)
-            if msg_length:
-                msg_length = int(msg_length)
-                msg = conn.recv(msg_length).decode(FORMAT)
-                if msg == EXIT:
-                    connected = False
-                else:
-                    taxi_warning(msg, sensor_id)
-                    conn.send(f"TAXI has been warned".encode(FORMAT))
-        print(f"CLOSING THE SENSOR NUMBER {sensor_id}")
-        conn.close()
-
-
-    def start():
-        server.listen()
-        sensor_id = 1
-
-        while True:
-            conn, addr = server.accept()
-            thread = threading.Thread(target=handle_sensor, args=(conn, sensor_id))
-            thread.start()
-            num_of_sensors = threading.active_count() - 2
-            sensor_id+=1
-            print(f"Number of sensors: {num_of_sensors}")
 
     ####################################################
 
